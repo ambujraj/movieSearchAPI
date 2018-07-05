a56753f73b3abb6f1282aba6332e3514f291b0bd
Ambuj <thecoderguy@outlook.com>
2018-07-05 19:53:13 +0530

    major

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const request = require('request');
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+  res.render("search");
+});
+app.get("/results", function(req, res){
+    var query = req.query.query;
+    var url = 'https://www.omdbapi.com/?s='+query+'&apikey=thewdb';
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var parsed = JSON.parse(body);
+            res.render("result", {name: parsed});
+                  
+        }
+    });
+   
+});
+
+app.listen(3000, function(){
+    console.log("Server started at port 3000");
+});
